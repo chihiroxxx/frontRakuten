863d4a3cf5ff02b4c96dc9bab12344e5a85ac475
chihiroxxx <maturi1000@gmail.com>
2021-08-08 00:09:19 +0900

    origins戻した

--- src/providers/Provider.jsx
+++ src/providers/Provider.jsx
@@ -21,7 +21,7 @@ export const MainProvider = (props) => {
 
   const [ loginFlag, setLoginFlag ] = useState(false)
 
-  const railsUrl = "http://10.0.60.10:3000"
+  const railsUrl = "http://ip-10-0-60-10.ap-northeast-1.compute.internal:3000"
 
   return (
     <MainContext.Provider value={{ testName, data, setData, text, setText,
